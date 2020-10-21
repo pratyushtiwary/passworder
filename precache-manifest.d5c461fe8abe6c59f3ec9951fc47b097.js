09cd10405a5b0d75ba8336f29db050f9b3ed81fe
= <=>
2020-10-21 11:14:29 +0530

    Fixed some bugs

---
+++ precache-manifest.d5c461fe8abe6c59f3ec9951fc47b097.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabdee9c7f41ee685e593f9b929bacab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ef020a03093300c3056",
+    "url": "./static/css/main.15713b75.chunk.css"
+  },
+  {
+    "revision": "af6130af21b7c3860a1a",
+    "url": "./static/js/2.7b1f6058.chunk.js"
+  },
+  {
+    "revision": "c18ad82b3be9985ffcbc3de3c04c0198",
+    "url": "./static/js/2.7b1f6058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef020a03093300c3056",
+    "url": "./static/js/main.556f24b0.chunk.js"
+  },
+  {
+    "revision": "063972711c492396cab8",
+    "url": "./static/js/runtime-main.189d4ba4.js"
+  },
+  {
+    "revision": "74bde32c3de41cc52b4d380984e443fe",
+    "url": "./static/media/empty.74bde32c.svg"
+  }
+]);
